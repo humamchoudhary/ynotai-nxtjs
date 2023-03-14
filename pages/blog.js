cc4d732626007534476cd5d8cfe0330a8f13f4f8
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-03-14 23:11:48 +0500

    update-services

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,7 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Layout from "../components/Layout";
-import { Typography, Space, Card } from "antd";
+import { Typography,  Card } from "antd";
 
 export default function Blog() {
   const blogs = [
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,82 @@ import Layout from "../components/Layout";
 import Image from "next/image";
 import { Typography, Space } from "antd";
 import { colors } from "../public/utilities";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+const AliceCarousel = dynamic(() => import("react-alice-carousel"), {
+  ssr: false,
+});
+import "react-alice-carousel/lib/alice-carousel.css";
+const responsive = {
+  0: {
+    items: 1,
+  },
+  600: {
+    items: 3,
+  },
+  1024: {
+    items: 5,
+  },
+};
 const myLoader = () => {
   return "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png";
 };
 export default function App() {
+  const services = [
+    "Service 1",
+    "Service 2",
+    "Service 3",
+    "Service 4",
+    "Service 5",
+    "Service 6",
+    "Service 7",
+    "Service 8",
+  ];
+
+  const serviceCards = services.map((service) => {
+    return (
+      <div key={service}>
+        <div style={{ position: "relative" }}>
+          <p
+            style={{
+              position: "absolute",
+              left: "20px",
+              color: colors.CTA,
+              fontWeight: 800,
+              zIndex: 20,
+            }}
+          >
+            {service}
+          </p>
+          <div
+            style={{
+              backgroundColor: colors.Dark,
+              width: "200px",
+              borderRadius: "5px",
+              height: "300px",
+            }}
+          >
+            <Image
+              width={200}
+              height={300}
+              draggable={true}
+              style={{
+                borderRadius: "5px",
+                opacity: 0.5,
+              }}
+              src="/images/service1.jpg"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  });
   const { Title } = Typography;
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
   return (
     <Layout main="Get your work done" span="Professionally">
       <div
@@ -122,6 +193,40 @@ export default function App() {
               }}
             />
           </Space>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "2rem",
+          }}
+        >
+          <Title
+            style={{
+              color: colors.Dark,
+              fontSize: "4rem",
+              fontWeight: "900",
+              textAlign: "center",
+            }}
+          >
+            Our Services
+          </Title>
+          {loaded && (
+            <AliceCarousel
+              mouseTracking
+              items={serviceCards}
+              responsive={responsive}
+              autoPlay
+              autoPlayInterval={1000}
+              animationDuration={1000}
+              disableButtonsControls
+              infinite
+              touchTracking={true}
+            />
+          )}
         </div>
       </div>
     </Layout>
